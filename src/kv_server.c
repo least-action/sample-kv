89a8a04495522946623d6737c6241fb4ae1d2fea
least-action <taewoo.kim.f.r@gmail.com>
2025-01-28 09:06:05 +0000

    feat: separate server code

---
+++ src/kv_server.c
@@ -0,0 +1,148 @@
+#include "command.h"
+#include "kv_hash.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/epoll.h>
+#include <stddef.h>
+
+#define BUFFER_SIZE 64
+#define RESULT_SIZE 64
+#define MAX_EVENTS 64
+
+int kv_run_server (uint16_t port)
+{
+     int server_fd, client_fd;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_len = sizeof (client_addr);
+    char buffer[BUFFER_SIZE];
+    char result[RESULT_SIZE];
+    size_t result_len;
+
+    struct epoll_event set_event;
+    int epfd;
+    struct epoll_event *events;
+    int e_count;
+
+    server_fd = socket (AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        perror ("failed to create server socket");
+        exit (EXIT_FAILURE);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
+
+    if (bind (server_fd, (struct sockaddr*)&server_addr, sizeof (server_addr)) < 0) {
+        perror ("failed to bind");
+        close (server_fd);
+        exit (EXIT_FAILURE);
+    }
+
+    if (listen (server_fd, 1) < 0) {
+        perror ("failed to listen");
+        close (server_fd);
+        exit (EXIT_FAILURE);
+    }
+
+    epfd = epoll_create1(EPOLL_CLOEXEC);
+    if (epfd < 0) {
+        perror ("epoll_create1 error");
+        close (server_fd);
+        exit (EXIT_FAILURE);
+    }
+
+    set_event.data.fd = server_fd;
+    set_event.events = EPOLLIN;
+    if (epoll_ctl (epfd, EPOLL_CTL_ADD, server_fd, &set_event)) {
+        perror ("epoll_ctl error");
+        close (epfd);
+        close (server_fd);
+        exit (EXIT_FAILURE);
+    }
+
+    events = malloc (sizeof(struct epoll_event) * MAX_EVENTS);
+    if (!events) {
+        perror ("epoll malloc");
+        free (events);
+        close (epfd);
+        close (server_fd);
+        exit (EXIT_FAILURE);
+    }
+
+    printf ("server is running on port %d\n", port);
+
+    struct kv_ht *ht;
+    ht = kv_ht_create (2);
+
+    while (1) {
+        e_count = epoll_wait (epfd, events, MAX_EVENTS, -1);
+        if (e_count < 0) {
+            perror ("epoll_wait");
+            break;
+        }
+
+        for (int i = 0; i < e_count; ++i) {
+            if (events[i].data.fd == server_fd) {
+                client_fd = accept (server_fd, (struct sockaddr*)&client_addr, &addr_len);
+                 if (client_fd < 0) {
+                    perror("failed to accept");
+                    continue;
+                }
+
+                set_event.data.fd = client_fd;
+                set_event.events = EPOLLIN;
+                epoll_ctl (epfd, EPOLL_CTL_ADD, client_fd, &set_event);
+
+                printf ("client connected from: %s:%d\n",
+                    inet_ntoa (client_addr.sin_addr),
+                    ntohs (client_addr.sin_port)
+                );
+
+                if (write (client_fd, "connected\r\n", 11) < 11) {
+                    perror ("failed to write");
+                    epoll_ctl (epfd, EPOLL_CTL_DEL, client_fd, NULL);
+                    close (client_fd);
+                    printf ("client disconnected\n");
+                }
+            }
+           else {
+                client_fd = events[i].data.fd;
+                memset (buffer, 0, BUFFER_SIZE);
+                // todo: when user send more than BUFFER_SIZE
+                int bytes_read = read (client_fd, buffer, BUFFER_SIZE - 1);
+                if (bytes_read <= 0) {
+                    epoll_ctl (epfd, EPOLL_CTL_DEL, client_fd, NULL);
+                    close (client_fd);
+                    printf ("client disconnected\n");
+                    continue;
+                }
+
+                // todo: when received data partially
+                buffer[bytes_read] = '\0';
+                run_command(ht, buffer, result);
+                if (result[0] != '\0')
+                    strcpy(result + strlen(result), "\r\n");
+                result_len = strlen(result) + 1;
+
+                if (write (client_fd, result, result_len) < result_len) {
+                    perror ("failed to write");
+                    epoll_ctl (epfd, EPOLL_CTL_DEL, client_fd, NULL);
+                    close (client_fd);
+                    printf ("client disconnected\n");
+                }
+            }
+        }
+    }
+
+    free (events);
+    close (epfd);
+    close (server_fd);
+
+    return 0;
+}
+
---
+++ src/kv_server.h
@@ -0,0 +1,9 @@
+#ifndef __KV_SERVER_H__
+#define __KV_SERVER_H__
+
+#include <stdint.h>
+
+int kv_run_server (uint16_t port);
+
+#endif
+
